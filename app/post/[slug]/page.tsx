d93de349d66454cc37768ef4d9eaefb8002e0d45
udonc <28778487+udonc@users.noreply.github.com>
2025-10-21 14:25:58 +0900

    add post page

---
+++ app/post/[slug]/page.tsx
@@ -0,0 +1,10 @@
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const { default: Post } = await import(`@/content/${slug}.md`);
+
+  return <Post />;
+}
