5a427f16bb4ae2a83594312e980fed3ef94cc978
udonc <28778487+udonc@users.noreply.github.com>
2025-10-21 23:44:29 +0900

    add generateMetadata return type

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,8 +1,10 @@
+import type { Metadata } from "next";
+
 type Props = {
   params: Promise<{ slug: string }>;
 };
 
-export async function generateMetadata({ params }: Props) {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { slug } = await params;
   const { frontmatter } = (await import(`@/content/${slug}.md`)) as {
     default: React.ComponentType;
