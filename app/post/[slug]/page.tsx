b4a058d0163987b6df8e65a00719e06b19cd9821
udonc <28778487+udonc@users.noreply.github.com>
2025-10-21 14:52:05 +0900

    fix post page

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,10 +1,26 @@
-export default async function Page({
-  params,
-}: {
+type Props = {
   params: Promise<{ slug: string }>;
-}) {
+};
+
+export async function generateMetadata({ params }: Props) {
   const { slug } = await params;
-  const { default: Post } = await import(`@/content/${slug}.md`);
+  const { frontmatter } = (await import(`@/content/${slug}.md`)) as {
+    default: React.ComponentType;
+    frontmatter: { title?: string; description?: string };
+  };
+
+  return {
+    title: frontmatter.title ?? "Untitled Post",
+    description: frontmatter.description,
+  };
+}
+
+export default async function Page({ params }: Props) {
+  const { slug } = await params;
+  const { default: Post } = (await import(`@/content/${slug}.md`)) as {
+    default: React.ComponentType;
+    frontmatter: { title?: string; description?: string };
+  };
 
   return <Post />;
 }
